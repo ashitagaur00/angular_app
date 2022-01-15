ed181a730c5338a0c2bbf5e1dd021c72788fc92d
Ashita Gaur <33507755+ashitagaur00@users.noreply.github.com>
2022-01-15 22:53:46 +0530

    material design added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MaterialModule } from './material.module';
+import {MatIconModule} from '@angular/material/icon';
+// import { MatButtonModule,  MatIconModule } from '@angular/material';
+// MatCheckboxModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatMenuModule,
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // MaterialModule,
+    // MatButtonModule,
+     MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
